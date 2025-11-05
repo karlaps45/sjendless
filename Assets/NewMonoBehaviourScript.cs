2f60c917431c68aa8d9aadc2b74ad921e740b3ac
karlaps45 <140808213+karlaps45@users.noreply.github.com>
2025-11-05 14:36:55 +0100

    YEAHHH

--- Assets/NewMonoBehaviourScript.cs
+++ Assets/NewMonoBehaviourScript.cs
@@ -1,11 +1,12 @@
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class ClairController : MonoBehaviour
 {
     [SerializeField] float vbegin = 4f;
-    [SerializeField] float tmax = 1.667f;
-    float g;
+    [SerializeField] float tmax = 0.867f;
+    float g = -5f;
 
     Animator animator;
 
@@ -22,62 +23,43 @@ public class ClairController : MonoBehaviour
         animator = GetComponent<Animator>();
 
         // Bereken g op basis van vbegin en tmax
-        g = (-2 * vbegin) / tmax;
-        acceleration = new Vector3(0, g, 0);
+        //g = (-2 * vbegin) / tmax;
+        //acceleration = new Vector3(0, g, 0);
 
-        // Start in running state
-        SetAnimationState(true, false);
+        
     }
 
     void Update()
     {
-        // Gebruik het nieuwe Input System
-        if (Keyboard.current.spaceKey.wasPressedThisFrame ||
-            Mouse.current.leftButton.wasPressedThisFrame)
-        {
-            HandleJumpInput();
-        }
-
-        HandleJumpState();
-    }
-
-    void HandleJumpInput()
-    {
         if (myState == State.running)
         {
-            myState = State.jumping;
-            SetAnimationState(false, true);
+            if (Input.GetMouseButtonDown(0))
+            {
+                Debug.Log("het werkt");
+                myState = State.jumping;
+                velocity = new Vector3(0, vbegin, 0);
+                acceleration = new Vector3(0, g, 0);
+                t = 0f;
+            }
 
-            t = 0f;
-            velocity = new Vector3(0, vbegin, 0);
         }
-    }
 
-    void HandleJumpState()
-    {
         if (myState == State.jumping)
         {
             t += Time.deltaTime;
-            velocity += acceleration * Time.deltaTime;
-            transform.position += velocity * Time.deltaTime;
-
-            if (t >= tmax)
+            if (t > tmax)
             {
-                myState = State.running;
-                SetAnimationState(true, false);
-
                 velocity = Vector3.zero;
-                transform.position = new Vector3(transform.position.x, 0, transform.position.z);
+                acceleration = Vector3.zero;
+                myState = State.running;
+
             }
         }
+
+        velocity += acceleration * Time.deltaTime;
+        transform.position += velocity * Time.deltaTime;
+
     }
 
-    void SetAnimationState(bool running, bool jumping)
-    {
-        if (animator != null)
-        {
-            animator.SetBool("running", running);
-            animator.SetBool("jumping", jumping);
-        }
-    }
+    
 }
